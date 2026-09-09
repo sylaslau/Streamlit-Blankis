e97ce352a7ed27462a1aef9fa103631c118f0d73
sylaslau <sylaslau@gmail.com>
2026-09-09 10:20:26 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-st.write("Aloooooha")
-st.write("shows me the updates")
+
+st.write("Guess my fav flav")
+
+import streamlit as st
+
+if st.button("Citrus"):
+    st.write("Nope")
+
+if st.button("Smokey"):
+    st.write("Bingo")
