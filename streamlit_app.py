060532b8806af286192b8627fbe26b80911bbac9
sylaslau <sylaslau@gmail.com>
2026-09-09 10:34:35 +0000

    list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,17 @@ import streamlit as st
 
 st.title("🎈 My new app")
 
-st.write("Guess my fav flav")
+st.write("Guess my fav direction of flav for cocktails")
 
 import streamlit as st
 
-if st.button("Citrus"):
-    st.write("Nope")
+option = st.selectbox(
+    "Choose a flavor profile:",
+    ["Smokey", "Fruity", "Floral", "Bitter", "Woody"]
+)
+if st.button("Submit Guess"):
+    if option == "Woody":
+        st.success("Bingo!")
+    else:
+        st.error("Nope")
 
-if st.button("Smokey"):
-    st.write("Bingo")
