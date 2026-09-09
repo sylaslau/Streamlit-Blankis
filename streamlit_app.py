7daf500b5379df7d9da880ad88cbd7f200785b2c
sylaslau <sylaslau@gmail.com>
2026-09-09 12:02:23 +0000

    add emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,25 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🍸 Mix & Shake")
 
-st.write("Guess my fav direction of flav for cocktails")
+st.write("What is your fav flav profile for cocktails")
 
 import streamlit as st
 
-option = st.selectbox(
-    "Choose a flavor profile:",
-    ["Smokey", "Fruity", "Floral", "Bitter", "Woody"]
+option = st.radio(
+    "Pick one:",
+    ["Fruity", "Floral", "Herbal", "Creamy", "Spicy"]
 )
-if st.button("Submit Guess"):
-    if option == "Woody":
-        st.success("Bingo!")
-    else:
-        st.error("Nope")
+if option == "Fruity":
+    st.success("🍓")
+elif option == "Floral":
+    st.success("🌸")
+elif option == "Herbal":
+    st.success("🍀")
+elif option == "Spicy":
+    st.success("🌶")
+elif option == "Creamy":
+    st.success("🍦")
 
+if st.button("Let's start with this"):
+    st.success("🥂")
