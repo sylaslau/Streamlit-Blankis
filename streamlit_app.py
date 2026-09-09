40d97b562ac984c7eafc294b70e6118dc06cd5d9
sylaslau <sylaslau@gmail.com>
2026-09-09 08:50:51 +0000

    1st deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write("Aloooooha")
+st.write("shows me the updates")
