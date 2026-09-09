381d5e04e351e3cf73e50f94710f605f4a4d29a2
sylaslau <sylaslau@gmail.com>
2026-09-09 14:16:12 +0000

    change topic

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,23 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🍸 Mix & Shake")
+st.title("😸 All the kitties in the cartoon world 😸")
 
-st.write("Guess what is my fav flav profile for cocktails")
+st.write("Guess my favourite cartoon cat charater")
 
 import streamlit as st
 
-profile = st.radio(
-    "Pick one:",
-    ["Fruity", "Floral", "Herbal", "Creamy", "Spicy"]
-)
-if profile == "Fruity":
-    st.success("🍓")
-elif profile == "Floral":
-    st.error("🌸")
-elif profile == "Herbal":
-    st.error("🍀")
-elif profile == "Spicy":
-    st.error("🌶")
-elif profile == "Creamy":
-    st.error("🍦")
+list_cat = ["Garfield","Hello Kitty","Puss in Boots", "Cheshire Cat", "Doraemon", "Marie"]
+
+cat_df = pd.DataFrame ({"sth": list_cat,
+                    "year_fam": [1987, 1974, 2004, 1865, 1969, 1970],
+                    "attitude": ["Sarcastic","Cute", "Adventurous", "Mysterious", "Intelligent", "Spolied"]
+                    })
 
 
-sweet = st.select_slider(
-    "On a scale of 1 to 5, how sweet do you like your cocktail?",
-    options=[1, 2, 3, 4, 5]
-)
+with st.expander("open for df"):
+    st.dataframe(cat_df)
 
-strength = st.select_slider(
-    "And how boozy do you want it to be?",
-    options=[0, 1, 2, 3, 4, 5]
-)
+#q1 = "What attitude style do you think this cat has?"
 
-if st.button("Let's start with this"):
-    st.success("🥂")
-
-
-
-#cate = ["a", "b", "c", "d","e"]
-#cate_df = pd.DataFrame({
-#    "profile": 
-#})
-
-
+#q2 = "When did this cat become famous"
